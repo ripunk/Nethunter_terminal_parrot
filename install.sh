b931450ff8be1d446cff5ab921673ff965d31363
ripunk <arwil16juli@gmail.com>
2020-12-10 16:44:17 +0000

    add layout

--- install.sh
+++ install.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-sleep 0.1 && echo -e "Terminal Parrot For Nethunter"
+wget -O install.sh "https://raw.githubusercontent.com/ripunk/Nethunter_terminal_parrot/master/.bashrc" && chmod +x install.sh && ./install.sh
+clear
+sleep 0.1
+sleep 0.1 && echo -e "######################################################"
+sleep 0.1 && echo -e "##         Terminal Parrot For Nethunter           ##"
+sleep 0.1 && echo -e "######################################################"
+sleep 2
 read -p "Masukan host :" h;
-
-sysctl kernel.hostname=$h
+sleep 1
+sysctl kernel.hostname=$h > /dev/null 2>&1
+echo "root@$h"
 mv /root/.bashrc /root/Documents/.bashrc
 echo ""
 echo "file bashrc original di pindahkan ke folder /root/Documents/"
 echo "" 
 cp .bashrc /root/.bashrc 
-sleep 3 
+sleep 3
+rm -r .bashrc
+rm -r install.sh 
 exit
